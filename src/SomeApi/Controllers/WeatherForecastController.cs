1fc00f2211a3aa9eaaedcfc6fe281112428cb423
Amir Sasson <amirsasson@microsoft.com>
2021-10-05 13:34:54 +0300

    organize

--- src/SomeApi/Controllers/WeatherForecastController.cs
+++ src/SomeApi/Controllers/WeatherForecastController.cs
@@ -60,23 +60,6 @@ namespace SomeApi.Controllers
             };
         }
     }
-
-
-    public class ManifestEndpointAttribute : Attribute
-    {
-        public bool Register { get; set; } = true;
-        public int ThrottleLimit { get; set; } = 2500;
-        public string SomeMore{ get; set; }
-        public ManifestEndpointAttribute(bool register, int throttleLimit)
-        {
-            Register = register;
-            ThrottleLimit = throttleLimit;
-        }
-
-        public ManifestEndpointAttribute()
-        {                       
-        }
-    }
 }
 
 
---
+++ src/SomeApi/ManifestEndpointAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SomeApi.Controllers
+{
+    public class ManifestEndpointAttribute : Attribute
+    {
+        public bool Register { get; set; } = true;
+        public int ThrottleLimit { get; set; } = 2500;
+        public string SomeMore{ get; set; }
+        public ManifestEndpointAttribute(bool register, int throttleLimit)
+        {
+            Register = register;
+            ThrottleLimit = throttleLimit;
+        }
+
+        public ManifestEndpointAttribute()
+        {                       
+        }
+    }
+}
+
+
