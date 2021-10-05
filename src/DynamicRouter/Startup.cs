a3a1bee4cbeb10b83249306a9821c6bd672f8eca
Amir Sasson <amirsasson@microsoft.com>
2021-10-05 15:15:24 +0300

    typo

--- src/DynamicRouter/Startup.cs
+++ src/DynamicRouter/Startup.cs
@@ -35,7 +35,7 @@ namespace DynamicRoutes
               "WWW",
               o =>
               {
-
+                  o.SkipClientCertificateChecks = false;
               });
         }
 
@@ -59,56 +59,10 @@ namespace DynamicRoutes
 
             app.UseManifestEndpointMiddleware();
 
-            //app.Use(async (context, next) =>
-            //{               
-            //    var authenticationService = context.RequestServices.GetService<IAuthenticationService>();
-            //    var authenticationResult = await authenticationService.AuthenticateAsync(context, "WWW");
-
-            //    var authorized = authenticationResult.Succeeded;
-
-            //    var cleanedPath = context.Request.Path.ToClean();
-
-            //    if (TryGetEndpoint(context, out var endpoint))
-            //    {
-            //        HttpClient c = new HttpClient();
-            //        UriBuilder b = new UriBuilder("http", "localhost", endpoint.Port, cleanedPath);
-            //        b.Query = context.Request.QueryString.ToString();
-            //        using var req = new HttpRequestMessage(new HttpMethod(context.Request.Method), b.Uri) {/* body.. headers .. more */};
-            //        var resp = await c.SendAsync(req);
-            //        context.Response.StatusCode = (int)resp.StatusCode;
-            //        await context.Response.WriteAsync(await resp.Content.ReadAsStringAsync());
-            //        return;
-            //    }
-            //    else
-            //    {
-            //        // Call the next delegate/middleware in the pipeline
-            //        await next();
-            //    }
-            //});
-
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
-
-        //private static bool TryGetEndpoint(HttpContext context, out ApiEndpoint endpoint)
-        //{
-        //    endpoint = null;
-        //    var key = Controllers.EndpointsManifestController.ManifestDB.Keys.FirstOrDefault(pattern =>
-        //    {
-        //        var template = TemplateParser.Parse(pattern);
-        //        var matcher = new TemplateMatcher(template, new RouteValueDictionary());
-
-        //        var isMatch = matcher.TryMatch(context.Request.Path, new RouteValueDictionary());
-        //        return isMatch;
-        //    });
-        //    if (key != null)
-        //    {
-        //        endpoint = Controllers.EndpointsManifestController.ManifestDB[key];
-        //    }
-        //    return endpoint != null;
-        //}
     }
 }
